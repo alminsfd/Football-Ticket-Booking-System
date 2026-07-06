8b39ab8baf78701a59287929829e2818d4dc3a80
alminsfd <alaminhossaintanvir42@gmail.com>
2026-07-07 02:06:40 +0600

    match tables schima done

--- SolveQuries.sql
+++ SolveQuries.sql
@@ -7,6 +7,8 @@ DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
 
+--- users table
+
 CREATE TABLE Users (
     user_id      SERIAL PRIMARY KEY,
     full_name    VARCHAR(100) NOT NULL,
@@ -15,3 +17,14 @@ CREATE TABLE Users (
     phone_number VARCHAR(20)
 );
 
+ --- matches table
+CREATE TABLE Matches (
+    match_id            SERIAL PRIMARY KEY,
+    fixture             VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price   NUMERIC(10, 2) NOT NULL,
+    match_status        VARCHAR(20) NOT NULL, 
+    
+    CONSTRAINT chk_positive_price CHECK (base_ticket_price >= 0),
+    CONSTRAINT chk_match_status CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
